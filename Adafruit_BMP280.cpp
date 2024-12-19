19740ef342f23982dded3093039adfb1f5879b3a
Esteban Fuentealba <efuentealba@json.cl>
2024-12-19 01:32:20 -0300

    Update Adafruit_BMP280.cpp

--- Adafruit_BMP280.cpp
+++ Adafruit_BMP280.cpp
@@ -457,9 +457,9 @@ bool Adafruit_BMP280_Temp::getEvent(sensors_event_t *event) {
     @brief  Gets the sensor_adafruit_t data for the BMP280's pressure sensor
 */
 /**************************************************************************/
-void Adafruit_BMP280_Pressure::getSensor(sensor_t *sensor) {
-  /* Clear the sensor_t object */
-  memset(sensor, 0, sizeof(sensor_t));
+void Adafruit_BMP280_Pressure::getSensor(sensor_adafruit_t *sensor) {
+  /* Clear the sensor_adafruit_t object */
+  memset(sensor, 0, sizeof(sensor_adafruit_t));
 
   /* Insert the sensor name in the fixed length char array */
   strncpy(sensor->name, "BMP280", sizeof(sensor->name) - 1);
